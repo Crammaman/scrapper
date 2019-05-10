42a74422cec2e607a3be4e724e21c2d4afee6271
crammaman <smadams00@gmail.com>
2019-05-10 15:05:53 +1000

    Set up whenever with it's own run task

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,24 +1,6 @@
 # Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-set :whenever_command_environment_variables, ->{ fetch(:default_env) }
+job_type :run_task,  "cd :path && bundle exec script/:task :output"
 every :day, at: '4:00 am' do
-  script 'scrapper'
+  run_task 'scrapper'
 end
-
-# Learn more: http://github.com/javan/whenever
