44c3f9fa88778dba83379567592e9958908f0f18
crammaman <smadams00@gmail.com>
2019-05-10 15:08:28 +1000

    corrected scrapper task for whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 # Use this file to easily define all of your cron jobs.
 
-job_type :run_task,  "cd :path && bundle exec script/:task :output"
+job_type :scrapper,  "cd :path && bundle exec ./scrapper :task :output"
 every :day, at: '4:00 am' do
-  run_task 'scrapper'
+  scrapper ''
 end
