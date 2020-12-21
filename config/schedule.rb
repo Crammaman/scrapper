161c1e5037f00fca45c2b2947b6f98f1df299048
Sam <smadams00@gmail.com>
2020-12-21 16:08:44 +1100

    Christmas signature update, removed Aiden from the list.

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,6 +5,9 @@ every :day, at: '2:00 am' do
   scrapper 'export_to_domo'
 end
 
+every 1.month, :at => 'December 22nd 2:00am' do
+  scrapper 'update_signatures'
+end
 # every :day, at: '6:00 pm' do
 #   scrapper 'bhp_blackwoods_references'
 # end
