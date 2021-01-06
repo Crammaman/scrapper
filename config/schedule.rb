72589e18112b5b8367779c3a7d04abf19f6a1c5d
Sam <smadams00@gmail.com>
2021-01-06 12:04:18 +1100

    NY2021 signature update, removed scheduled sig change.

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,10 +4,6 @@ job_type :scrapper,  "cd :path && bundle exec ./scrapper :task :output"
 every :day, at: '2:00 am' do
   scrapper 'export_to_domo'
 end
-
-every 1.month, :at => 'December 22nd 2:00am' do
-  scrapper 'update_signatures'
-end
 # every :day, at: '6:00 pm' do
 #   scrapper 'bhp_blackwoods_references'
 # end
